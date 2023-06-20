f120df21e61fa655915ddfa189d6748a5793f7c5
Heliospook <subhajeetlahiri@gmail.com>
2023-06-20 11:52:58 +0530

    bug fixes in api-gateway

--- server/api-gateway/src/main/java/com/example/apigateway/config/ApiGatewayConfiguration.java
+++ server/api-gateway/src/main/java/com/example/apigateway/config/ApiGatewayConfiguration.java
@@ -1,10 +1,4 @@
 package com.example.apigateway.config;
-
-import com.example.apigateway.filters.AuthenticationFilter;
-import org.apache.hc.client5.http.auth.AuthStateCacheable;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.cloud.gateway.filter.GatewayFilter;
-import org.springframework.cloud.gateway.route.Route;
 import org.springframework.cloud.gateway.route.RouteLocator;
 import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
 import org.springframework.context.annotation.Bean;
@@ -12,8 +6,6 @@ import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class ApiGatewayConfiguration {
-    @Autowired
-    private AuthenticationFilter authenticationFilter;
     @Bean
     public RouteLocator gatewayRouter(RouteLocatorBuilder builder){
         return builder.routes()
